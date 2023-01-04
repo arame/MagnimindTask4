99d5d07ee14382eca772987717fb027bfb5f2414
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2023-01-04 13:45:34 +0000

    Put in Logistic Regression

--- _utility.py
+++ _utility.py
@@ -2,6 +2,8 @@ class gl:
     pkl_df_text = "pkl_df_text.pkl"
     pkl_df_is_business = "pkl_df_is_business.pkl"
     pkl_X_tfidf = "pkl_X_tfidf.pkl"
+    pkl_classifier_LR = "pkl_classifier_LR.pkl"
+    pkl_classifier_SVM = "pkl_classifier_SVM.pkl"
     # column names: ['tweetID', 'crDate','edInput', 'editor', 'engages', 'isApproved', 'isEdNeed', 'isRT', 'likes', 'photoUrl', 'retweets', 'rtUsID', 'text', 'topicName', 'usFlwrs', 'usID', 'usName', 'videoUrl']
     columns_not_needed = ['tweetID', 'crDate','edInput', 'editor', 'engages', 'isApproved', 'isEdNeed', 'isRT', 'likes', 'photoUrl', 'retweets', 'rtUsID', 'usFlwrs', 'videoUrl']
     text = "text"
