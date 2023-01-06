9a0c58aa3c2cc21c7ae398fbeea9bc3b5c915523
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2023-01-06 14:33:18 +0000

    Put in tf/idx matrix for ngram, stem & lem for LR

--- _metrics.py
+++ _metrics.py
@@ -1,11 +1,12 @@
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, matthews_corrcoef
 import matplotlib.pyplot as plt
 
-def display_metrics(y_test, y_pred):
+def display_metrics(y_test, y_pred, title = None):
     target_names = ['Class 1', 'Class 2']
     cm = confusion_matrix(y_test, y_pred)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
     disp.plot()
+    plt.title(title)
     plt.show()
     cr = classification_report(y_test, y_pred, target_names=target_names)
     print(cr)
